973869079bef20b850c971e52f6e11602b8b21ca
berthiegouin <berthie.gouin-ferland@usherbrooke.ca>
2019-02-26 10:31:02 -0500

    subsitution du code script pour un fonction auto_correlation.m

--- Autocorrelation/Demo1_25_02_15h.m
+++ Autocorrelation/Demo1_25_02_15h.m
@@ -3,18 +3,18 @@ clc
 close all
 clearvars
 
-%% Enregistrement du son
-
-recObj = audiorecorder;
-
-disp('Start speaking.')
-recordblocking(recObj, 1);
-disp('End of Recording.');
-
-play(recObj);
-
-y = getaudiodata(recObj);
-plot(y)
+% %% Enregistrement du son
+% 
+% recObj = audiorecorder;
+% 
+% disp('Start speaking.')
+% recordblocking(recObj, 1);
+% disp('End of Recording.');
+% 
+% play(recObj);
+% 
+% y = getaudiodata(recObj);
+% plot(y)
 
 
 %% Autocorrelation
@@ -23,37 +23,14 @@ close all
 clearvars
 
 
-x = [1 0 -1 0];
-y = [1 0 -1 0];
+x = [1 0 -1 0 ];
+load('Sin_1k')
 
-x = [zeros(1,length(x)) x zeros(1,length(x))];
-auto_correlation(1:length(y)*2-1,:) = 0;
+y = y';
 
-for boucle_corr = 1:(length(y)*2-1)
-   y_corr = [zeros(1,boucle_corr) y zeros(1,length(y)*2-boucle_corr)];
-   auto_correlation(boucle_corr) = sum(x .* y_corr);
-end
+corr = auto_correlation(y);
 
-
-stem(auto_correlation)
-
-% max_autocorrelation = 0;
-% for i = 1:length(auto_correlation)
-%     if auto_correlation(i,:) > max_autocorrelation
-%         max_autocorrelation = auto_correlation(i,:);
-%     end
-% end
-% 
-% auto_correlation = auto_correlation ./  max_autocorrelation;
-
-
-
-
-
-
-
-
-
+stem(corr)
 
 
 
