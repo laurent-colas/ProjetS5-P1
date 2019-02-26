e0852ec8cbdf47af9218f5a150a44b88cd851ee6
berthiegouin <berthie.gouin-ferland@usherbrooke.ca>
2019-02-26 15:59:10 -0500

    nouvelles fonction en c

---
+++ correlation_VS/correlation/correlation/norm.h
@@ -0,0 +1,22 @@
+/*
+ * norm.h
+ *
+ *  Created on: Feb 26, 2019
+ *      Author: berth
+ */
+
+#ifndef SOURCE_HEADER_NORM_H_
+#define SOURCE_HEADER_NORM_H_
+#include "max.h"
+#include "abs.h"
+
+void norm(double x[], int l) {
+
+	absolute(x,l);
+	double AbsMax = max(x,l);
+	for (int i = 0; i < l; i++)
+	{
+		x[i] = x[i] / AbsMax;
+	}
+}
+#endif  /*SOURCE_HEADER_LENGTH_H_ */
