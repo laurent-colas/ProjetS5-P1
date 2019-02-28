6e24dbe2958c5ba7c5044ee28bec3f52b3ddfbf0
Pierre-Yan <pierre-yan.gingras@usherbrooke.ca>
2019-02-27 22:22:25 -0500

    tests

--- Demo_1/Matlab/Demo1_25_02_15h.m
+++ Demo_1/Matlab/Demo1_25_02_15h.m
@@ -7,8 +7,13 @@ clearvars
 t = 1:10;
 s_ref = sin(3.14.*t./10);
 
+figure
+stem(s_ref)
+
 [corr] = correlation(s_ref,s_ref,numel(s_ref));
 
-disp("Le resultat de la correlation est:")
+disp('Le resultat de la correlation est:')
 disp(['[ ' num2str(corr) ' ]'])
+
+
 
---
+++ Demo_1/Matlab/tests.m
@@ -0,0 +1,143 @@
+%% test1: deux signaux orthogonaux sans décalage
+clear all
+close all
+clc
+
+t = 1:100;
+
+s_ref = sin(3.14.*t./10);
+orto_ref = cos(3.14.*t./10);
+
+[corr] = correlation(s_ref,orto_ref,numel(s_ref));
+
+disp('Le resultat de la correlation est:')
+disp(['[ ' num2str(corr) ' ]'])
+
+maximum = max(abs(corr))
+
+figure
+stem(corr)
+
+%% test2 détectecte seulement voix humaine
+clear all
+close all
+clc
+
+recObj = audiorecorder;
+
+disp('Start speaking.')
+recordblocking(recObj, 1);
+disp('End of Recording.');
+
+play(recObj);
+
+y = getaudiodata(recObj);
+figure
+plot(y)
+
+pause(2);
+
+disp('Start noise.')
+recordblocking(recObj, 1);
+disp('End of Recording.');
+
+bruit = getaudiodata(recObj);
+
+figure
+plot(bruit)
+
+[corr] = correlation(y',bruit',numel(y));
+
+maximum = max(abs(corr))
+
+figure
+stem(corr)
+%% test3: deux signaux de longueur différente
+clear all
+close all
+clc
+
+t = 1:100;
+s_ref = sin(3.14.*t./10);
+petit_ref = s_ref;
+petit_ref(end-10:end)=0;%Retirer 10 derniers points et remplacer par des zéros
+ 
+figure
+plot(t,s_ref)
+
+figure
+plot(t,petit_ref)
+
+[corr] = correlation(s_ref,petit_ref,numel(s_ref));
+
+disp('Le resultat de la correlation est:')
+disp(['[ ' num2str(corr) ' ]'])
+
+maximum = max(abs(corr))
+
+figure
+stem(corr)
+
+%% test4: deux signaux identiques
+clear all
+close all
+clc
+
+t = 1:100;
+
+s_ref = sin(3.14.*t./10);
+
+[corr] = correlation(s_ref,s_ref,numel(s_ref));
+
+disp('Le resultat de la correlation est:')
+disp(['[ ' num2str(corr) ' ]'])
+
+maximum = max(abs(corr))
+
+figure
+stem(corr)
+
+%% test 5 deux signaux identiques décalés
+clear all
+close all
+clc
+
+t = 1:100;
+
+s_ref = sin(3.14.*t./10);
+s_ref_decal = sin(3.14.*t./10 -2);
+
+[corr] = correlation(s_ref,s_ref_decal,numel(s_ref));
+
+disp('Le resultat de la correlation est:')
+disp(['[ ' num2str(corr) ' ]'])
+
+maximum = max(abs(corr))
+
+figure
+stem(corr)
+%% test6
+clear all
+close all
+clc
+
+t = 1:100;
+
+s_ref = sin(3.14.*t./10);
+y = awgn(s_ref,20);
+
+[corr] = correlation(s_ref,y,numel(s_ref));
+
+disp('Le resultat de la correlation est:')
+disp(['[ ' num2str(corr) ' ]'])
+
+maximum = max((corr))
+
+figure
+plot(t,y)
+
+figure
+plot(t,s_ref)
+
+figure
+stem(corr)
